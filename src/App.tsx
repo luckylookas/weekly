db636415a0748102db109ba04ec25dc366075ab7
Lukas Noll <lukas.noll@untis.at>
2024-07-24 15:29:24 +0200

    deployment

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import React, {useCallback, useEffect, useState} from 'react';
-import {Button, Checkbox, createTheme, FormControlLabel} from "@mui/material";
+import {Button, Checkbox, FormControlLabel} from "@mui/material";
 
 
 interface week {
